9f1e705c4e03490659bfa40080cead3f9b77382c
Bowen Wang <wangb10@rpi.edu>
2019-02-23 16:55:22 -0500

    48 bit congruential linear random generator method found for java

--- CPU_Simulation/CPUSimulation/HelloWorld.java
+++ CPU_Simulation/CPUSimulation/HelloWorld.java
@@ -1,10 +1,18 @@
 package CPUSimulation;
+import java.util.Random;
 
 public class HelloWorld {
+	
+	public static int random(int seed) {
+		Random randomGenerator = new Random();
+		randomGenerator.setSeed(seed);
+		for(int i=0;i<20;i++)
+			System.out.println(randomGenerator.nextFloat()%1);
+		return 1;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		HelloWorld.random(5);
 	}
 
 }
