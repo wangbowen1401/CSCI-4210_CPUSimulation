b7b50df2f77a49718f6938fd7a7b385ab80a7379
Bowen Wang <wangb10@rpi.edu>
2019-03-01 22:38:25 -0500

    Algorithm pseudocode

---
+++ CPU_Simulation/CPUSimulation/Algorithms.java
@@ -0,0 +1,24 @@
+package CPUSimulation;
+
+import java.util.PriorityQueue;
+
+public class Algorithms {
+	// Need a queue for corresponding algorithm 
+	// 
+	
+	// Convert randomSequence into corresponding queue 
+	// Run simulation. Probably need two different constructors
+	public Algorithms(RandomSequence sequence) {
+		
+	}
+	
+	public void SRTSimulation(PriorityQueue<Event> events) {
+		
+	}
+	
+	
+	
+	
+	
+	
+}
