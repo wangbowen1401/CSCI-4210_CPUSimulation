2f2a14d03a5e89713561b1a9458d266bc0faecfb
Ryan <raor2@rpi.edu>
2019-03-12 20:18:47 -0400

    moved methods to Process

--- CPU_Simulation/cpuSimulation/Process.java
+++ CPU_Simulation/cpuSimulation/Process.java
@@ -48,9 +48,7 @@ public abstract class Process{
 		numContextSwitch=0;
 		cw = contextSwitch;
 	}
-	public abstract void enterQueue(double time);
-	public abstract void enterCPU(double time);
-	public abstract void complete(double time);
+
 	
 	
 	///////////////////////////////// Getters and Setters/////////////////////////////////
@@ -105,6 +103,39 @@ public abstract class Process{
 	public int getNumContextSwitch() {
 		return numContextSwitch;
 	}
+	
+	public void enterCPU(double time) {
+		this.state= "RUNNING";
+		if(enterTime!=-1)
+			waitTime[numCPUBurst-1]+=time-enterTime;
+		enterTime = time;// Refers to when the process enter the CPU
+	}
+	
+	public void enterQueue(double time) {
+		if(state == "RUNNING"&&enterTime!=-1) {
+			remainingTime -=(time-enterTime); 
+			numContextSwitch++;
+			numPreempt++;
+		}
+		state= "READY";
+		enterTime = time;
+	}
+	
+	// For SRT when CPU burst is complete
+	public void complete(double time) {
+		numCPUBurst--;
+		turnaroundTime[numCPUBurst]+=waitTime[numCPUBurst];
+		if(numCPUBurst>0) {
+			state="BLOCKED";
+			remainingTime = cpuBurstTime;
+			arrivalTime = ioBurstTime+time;
+			burstTimeGuess = (1-alpha)*burstTimeGuess+alpha*cpuBurstTime;
+		}
+		else 
+			state="COMPLETE";
+		numContextSwitch++;
+	}
+	
 	// String to show all the data
 	@Override
 	public String toString() {
