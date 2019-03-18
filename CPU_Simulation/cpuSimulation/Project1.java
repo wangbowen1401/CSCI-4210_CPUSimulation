a20add74f85a1b6125cfe20796c4b85ba48d1585
Ryan <raor2@rpi.edu>
2019-03-17 22:13:26 -0400

    added more print statements

--- CPU_Simulation/cpuSimulation/Project1.java
+++ CPU_Simulation/cpuSimulation/Project1.java
@@ -1,9 +1,19 @@
 package cpuSimulation;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class Project1{
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		//System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("out.txt"))));
+		PrintWriter writer  = new PrintWriter("simout.txt","UTF-8");
+		
 		double lambda =0.001;
 		double upper = 3000;
 		int n = 10;
@@ -11,20 +21,26 @@ public class Project1{
 		double cw = 8;
 		double alpha = 0.5;
 		RandomSequence test = new RandomSequence(seed,lambda,alpha,upper,n);
-		SRTAlgorithm SRT = new SRTAlgorithm(test,cw);
-		SRT.simulate();
+		//SRTAlgorithm SRT = new SRTAlgorithm(test,cw);
+		//SRT.simulate();
+		
 		//System.out.println(SRT);
 		//FCFSAlgorithm FCFS = new FCFSAlgorithm(test,alpha,cw);
 		//FCFS.simulate();
 		//System.out.println(FCFS);
-		//SJFAlgorithm SJF = new SJFAlgorithm(test, alpha, cw);
+		SJFAlgorithm SJF = new SJFAlgorithm(test, alpha, cw);
 
 		//FCFSAlgorithm FCFS = new FCFSAlgorithm(test,alpha,cw);
 		//FCFS.simulate();
 		//System.out.println(FCFS);
 
-		//SJF.simulate();
-		//System.out.println(SJF);
+		SJF.simulate();
+		System.out.println(SJF);
+		writer.println(SJF);
+		//	writer.println(SRT);
+		//writer.println(FCFS);
+		//writer.println(RR);
+		writer.close();
 		
 	}
 
